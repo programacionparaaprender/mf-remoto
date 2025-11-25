c738020fc6cb9f458f36a9fd77c2c16ee8cda76d
programacionparaaprender <yancel209@gmail.com>
2025-11-25 10:05:14 -0500

    corrigiendo index

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 
 import { Routes } from '@angular/router';
 import { RemotoComponent } from './views/remoto/remoto.component';
+import { ViejaComponent } from './views/vieja/vieja.component';
 
 export const routes: Routes = [
     { path: '',  redirectTo: '/masteraspnet', pathMatch: 'full' },
   {path: 'masteraspnet' , component: RemotoComponent},
-  {path: 'remoto' , component: RemotoComponent}
+  {path: 'remoto' , component: RemotoComponent},
+  {path: 'vieja' , component: ViejaComponent}
 ];
